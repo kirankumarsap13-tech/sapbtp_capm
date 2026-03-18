87aaed1fe39f11ad0bd1f2067eb4d72f5511d96b
Kiran K <kiran.kumarsap13@gmail.com>
2026-03-18 16:07:55 +0000

    Final Capmapp

---
+++ gen/srv/srv/CatalogService.js
@@ -0,0 +1,43 @@
+module.exports = cds.service.impl(async function(){
+    const {POs, EmployeesSet} = this.entities;
+    //Generic Handler - Developers gets flexibility to attache their 
+    // own code on top of what CAPM offers
+    this.before(['CREATE', 'PATCH'], EmployeesSet, (req) => {
+      if(parseFloat(req.data.salaryAmount) >= 1000000){
+        req.error(500,"Salary out of range");
+      }
+    });
+
+    this.on('boost', async(req) => { 
+        console.log("Boost");
+        try {
+            const ID = req.params[0];
+        // Start a DB Transaction
+        const tx = cds.tx(req);
+        //CDS Query Language - Communicate DB in agnostic manner
+        await tx.update(POs).with({
+            GROSS_AMOUNT: { '+=' : 20000 }
+        }).where(ID);            
+        } catch (error) {
+            return "Error " + error.toString();  
+        }
+
+    });
+    this.on('largestOrder', async(req) => { 
+        console.log("Boost");
+        try {
+            const ID = req.params[0];
+        // Start a DB Transaction
+        const tx = cds.tx(req); 
+        //Read PO with highest Gross Amount
+        //Sort PO in descending Order by GROSS AMOUNT and read the 1st record
+        const recordData =tx.read(POs).orderBy({
+            GROSS_AMOUNT: 'desc'
+        }).limit(1);          
+        } catch (error) {
+            return "Error " + error.toString();  
+        }
+
+    });
+
+});
